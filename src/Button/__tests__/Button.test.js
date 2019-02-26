2db25be30ba0631cd012e2d76a900598de347b41
Wade <wade.montague@gmail.com>
2019-02-26 09:40:00 +0000

    added more testing coverage

--- src/Button/__tests__/Button.test.js
+++ src/Button/__tests__/Button.test.js
@@ -6,3 +6,19 @@ test("it works", () => {
   const wrapper = renderer.create(<Button />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test("Outline variant", () => {
+  const wrapper = renderer.create(<Button outline />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("Rounded variant", () => {
+  const wrapper = renderer.create(<Button theme={{ enableRounded: true }} outline />);
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("Disable rounded variant", () => {
+  const wrapper = renderer.create(<Button theme={{ enableRounded: false }} outline />);
+  expect(wrapper).toMatchSnapshot();
+});
