3467a8277aaaea969d71e25da8bb4b36f69a318a
Wade <wade.montague@gmail.com>
2019-03-03 14:24:51 +0000

    lint tidy and added debugDefault

--- src/Grid/stories/Grid.story.js
+++ src/Grid/stories/Grid.story.js
@@ -1,9 +1,11 @@
+import { withInfo } from "@storybook/addon-info";
+import { boolean, withKnobs } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import React, { Fragment } from "react";
 import { host } from "storybook-host";
 import { Grid, GridItem } from "../Grid";
-import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
-import { withInfo } from "@storybook/addon-info";
+
+const debugDefault: boolean = true;
 
 export default storiesOf("Grids", module)
   .addDecorator(withInfo)
@@ -17,16 +19,16 @@ export default storiesOf("Grids", module)
   .add("Default", () => (
     <Fragment>
       <Grid gutter={50}>
-        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", false)}>
+        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", debugDefault)}>
           <p>Item 1</p>
         </GridItem>
-        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", false)}>
+        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1} debug={boolean("Debug", debugDefault)}>
           <p>Item 2</p>
         </GridItem>
-        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", false)}>
+        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", debugDefault)}>
           <p>Item 3</p>
         </GridItem>
-        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", false)}>
+        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", debugDefault)}>
           <p>Item 4</p>
         </GridItem>
       </Grid>
@@ -35,30 +37,30 @@ export default storiesOf("Grids", module)
   .add("Middle Center", () => (
     <Fragment>
       <Grid middle center>
-        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug debug={boolean("Debug", false)}>
+        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", debugDefault)}>
           <p>Item 1</p>
         </GridItem>
-        <GridItem col={3 / 4} debug={boolean("Debug", false)} />
-        <GridItem media={{ phone: 1, tablet: 1 }} col={1 / 4} debug={boolean("Debug", false)}>
+        <GridItem col={3 / 4} debug={boolean("Debug", debugDefault)} />
+        <GridItem media={{ phone: 1, tablet: 1 }} col={1 / 4} debug={boolean("Debug", debugDefault)}>
           <h1>Item 2</h1>
         </GridItem>
-        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", false)}>
+        <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4} debug={boolean("Debug", debugDefault)}>
           <Grid middle center>
-            <GridItem media={{ phone: 1 }} debug={boolean("Debug", false)}>
+            <GridItem media={{ phone: 1 }} debug={boolean("Debug", debugDefault)}>
               <p>Item 3a</p>
             </GridItem>
-            <GridItem media={{ phone: 1 }} debug={boolean("Debug", false)}>
+            <GridItem media={{ phone: 1 }} debug={boolean("Debug", debugDefault)}>
               <p>Item 3b</p>
             </GridItem>
-            <GridItem media={{ phone: 1 }} debug={boolean("Debug", false)}>
+            <GridItem media={{ phone: 1 }} debug={boolean("Debug", debugDefault)}>
               <p>Item 3b</p>
             </GridItem>
-            <GridItem media={{ phone: 1 }} debug={boolean("Debug", false)}>
+            <GridItem media={{ phone: 1 }} debug={boolean("Debug", debugDefault)}>
               <p>Item 3b</p>
             </GridItem>
           </Grid>
         </GridItem>
-        <GridItem media={{ phone: 1, tablet: 1 }} col={1 / 2} debug={boolean("Debug", false)}>
+        <GridItem media={{ phone: 1, tablet: 1 }} col={1 / 2} debug={boolean("Debug", debugDefault)}>
           <p>Item 4</p>
         </GridItem>
       </Grid>
