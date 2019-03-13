557fd83db6d6be48f1e66e252f22d8426d4706fa
Lusine <lusine@supergoteam.com>
2019-03-13 16:29:57 +0400

    adding testing for Card #mxwx2[READY FOR REVIEW]

---
+++ src/Card/__tests__/Card.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Card from "../Card";
+import CardImage from "../CardImage";
+
+test("create card", () => {
+    const wrapper = renderer.create(<Card />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("With Image", () => {
+    const wrapper = renderer.create(<CardImage />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("Card with Header", () => {
+    const wrapper = renderer.create(<Card.Header />);
+    expect(wrapper).toMatchSnapshot();
+});
