37fa29eb2b6e36a7ea2bd60b6defa6e5b5a199fd
Lusine <lusine@supergoteam.com>
2019-03-18 13:02:01 +0400

    Adding more functional for 100% coverage #mxwx1[READY FOR REVIEW]

--- src/Breadcrumbs/__tests__/Breadcrumbs.test.js
+++ src/Breadcrumbs/__tests__/Breadcrumbs.test.js
@@ -2,8 +2,12 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Breadcrumb from "../";
 
-test("breadcrums is created", () => {
+test("Breadcrums is created", () => {
     const wrapper = renderer.create(<Breadcrumb />);
     expect(wrapper).toMatchSnapshot();
 });
 
+test("Breadcrums item", () => {
+    const wrapper = renderer.create(<Breadcrumb.Item active />);
+    expect(wrapper).toMatchSnapshot();
+});
