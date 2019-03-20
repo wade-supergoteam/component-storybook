02824cda86ea0d057f4fcd4e4d6b1cfeb6f1d808
Lusine <lusine@supergoteam.com>
2019-03-20 12:28:50 +0400

    adding more coverage for Alert

--- src/Alert/__tests__/Alert.test.js
+++ src/Alert/__tests__/Alert.test.js
@@ -1,8 +1,19 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import DismissableAlert from "../DismissableAlert";
 import Alert from "../";
 
-test("it works", () => {
+test("Create Alert", () => {
     const wrapper = renderer.create(<Alert />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("Create DismissableAlert", () => {
+    const wrapper = renderer.create(<DismissableAlert type="primary" />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("Create DismissableAlert with onClose", () => {
+    const wrapper = renderer.create(<DismissableAlert type="primary" onClose={() => this.closeAlert("primary")} />);
     expect(wrapper).toMatchSnapshot();
 });
