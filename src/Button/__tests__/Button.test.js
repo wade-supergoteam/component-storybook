77ef2c2b542cadb720ba68bb8890f97412f50478
Lusine <lusine@supergoteam.com>
2019-03-29 14:31:05 +0400

    Adding 100% coverage for Button component

--- src/Button/__tests__/Button.test.js
+++ src/Button/__tests__/Button.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import Button from "../Button";
+import Button from "../";
 
 test("it works", () => {
   const wrapper = renderer.create(<Button />);
@@ -8,7 +8,7 @@ test("it works", () => {
 });
 
 test("Outline variant", () => {
-  const wrapper = renderer.create(<Button outline />);
+  const wrapper = renderer.create(<Button outline block  color="primary"/>);
   expect(wrapper).toMatchSnapshot();
 });
 
@@ -21,4 +21,31 @@ test("Rounded variant", () => {
 test("Disable rounded variant", () => {
   const wrapper = renderer.create(<Button theme={{ enableRounded: false }} outline />);
   expect(wrapper).toMatchSnapshot();
+});
+
+const margin = { marginRight: 6, marginBottom: 6 };
+
+test("Button with small size and margin", () => {
+    const wrapper = renderer.create(<Button style={margin} color="success" size="small" theme={{ enableRounded: true }} active />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("Button with large size and margin and enableRounded=true", () => {
+    const wrapper = renderer.create(<Button style={margin} color="success" size="large" theme={{ enableRounded: true }} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("Button with large size and margin and enableRounded=false", () => {
+    const wrapper = renderer.create(<Button style={margin} color="success" size="large" theme={{ enableRounded: false }} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("Button with small size and margin and enableRounded=false", () => {
+    const wrapper = renderer.create(<Button style={margin} color="success" size="small" theme={{ enableRounded: false }} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("Button color=link", () => {
+    const wrapper = renderer.create(<Button color="link" size="small" theme={{ enableRounded: false }} />);
+    expect(wrapper).toMatchSnapshot();
 });
