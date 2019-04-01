b3254125285f2cc2564769281afecd4bf621c945
Wade Montague <wademontague@Wades-MacBook-Pro.local>
2019-04-01 21:01:35 +0100

    added styled-media-responsive to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ import pkg from './package.json';
 export default [
   {
     input: 'src/index.js',
-    external: ['react', 'styled-components'],
+    external: ['react', 'styled-components', 'styled-media-responsive'],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
