9e08fb929872068052cfa334f7341cb5c4f08dc5
Andy Hall <andy@supergoteam.com>
2020-01-24 11:39:25 +0000

    Updated eslint imports

---
+++ src/Forms/stories/SelectTest.story.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import { storiesOf } from "@storybook/react";
+import { host } from "storybook-host";
+import { withKnobs, select } from "@storybook/addon-knobs/react";
+
+import AnimatedMulti from "../Select";
+
+export default storiesOf("Forms", module)
+  .addDecorator(withKnobs)
+  .addDecorator(
+    host({
+      align: "center",
+      width: 400,
+    }),
+  )
+  .add("SelectTest", () => (
+    <Fragment>
+      <AnimatedMulti />
+    </Fragment>
+  ));
